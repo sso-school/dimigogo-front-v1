87cdc02e8335c4fcbb9da68e7db4137d53d5248c
최재민 <admin@chicken-moo.com>
2023-05-10 12:35:23 +0900

    tsconfig

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,6 @@
 export const Colors = {
-  primary: '#A4CB73',
-  secondary: '#F4F5F1',
-  background: '#FDFFFA',
-  color: '#0C1600',
+  primary: "#A4CB73",
+  secondary: "#F4F5F1",
+  background: "#FDFFFA",
+  color: "#0C1600",
 };
